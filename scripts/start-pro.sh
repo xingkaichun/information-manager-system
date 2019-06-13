43eb4270c352da559e5e165cf347a9d6696d7e91
邢开春 <xingkaichun@ceair.com>
2019-06-13 18:19:34 +0800

    增加生产启动脚本与配置

---
+++ scripts/start-pro.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+echo "start at: " `date +%Y%m%d%k%M%S`
+
+## service name
+APP_NAME=information-manager-system
+SERVICE_NAME=information-manager-system-1.0-SNAPSHOT
+JAR_NAME=$SERVICE_NAME\.jar
+PID=$SERVICE_NAME\.pid
+
+program_dir=`dirname $0`
+if [ `echo "$0" |grep -c "/"` -le 0 ];then
+   program_dir=`pwd`
+else
+   program_dir=`cd ${program_dir};pwd`
+fi
+
+cd ${program_dir}
+
+echo `pwd`
+
+nohup java -Xms256M -Xmx1024M -Xss512k -XX:+HeapDumpOnOutOfMemoryError -XX:+DisableExplicitGC -XX:+UseParNewGC -XX:+UseConcMarkSweepGC -XX:+UseCMSCompactAtFullCollection -Xloggc:logs/gc.log -Dloader.path=libs/,config/,front-end/ -Dspring.profiles.active=pro -jar $JAR_NAME &
+
+echo "start ."
