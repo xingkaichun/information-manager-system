753c9837cc6a58cc1c72c725e1ae6179bf473f9f
xingkaichun <xingkaichun@ceair.com>
2019-06-17 13:49:35 +0800

    启动脚本更改

--- scripts/start-pro.sh
+++ scripts/start-pro.sh
@@ -5,7 +5,15 @@ echo "start at: " `date +%Y%m%d%k%M%S`
 APP_NAME=information-manager-system
 SERVICE_NAME=information-manager-system-1.0-SNAPSHOT
 JAR_NAME=$SERVICE_NAME\.jar
-PID=$SERVICE_NAME\.pid
+
+OLD_PID=`ps -ef | grep $JAR_NAME | grep -v grep | awk '{print $2}'`
+if [ "$OLD_PID" != "" ]; then
+	echo "Process already exist.Will kill it , and then start it！"
+	echo "Kill already process!PID:"$OLD_PID
+	kill $OLD_PID
+else
+	echo "process not exist.Will start it！"
+fi
 
 program_dir=`dirname $0`
 if [ `echo "$0" |grep -c "/"` -le 0 ];then
