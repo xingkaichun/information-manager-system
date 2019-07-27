a909cfab2e739895d5bb023e84dd68d43fbab4b7
xing kaichun <409060350@qq.com>
2019-07-27 18:42:38 +0800

    優化

--- src/main/java/com/xingkaichun/information/WebMvcConfig.java
+++ src/main/java/com/xingkaichun/information/WebMvcConfig.java
@@ -8,10 +8,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupp
 public class WebMvcConfig extends WebMvcConfigurationSupport {
     @Override
     protected void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("swagger-ui.html")
-                .addResourceLocations("classpath:/META-INF/resources/");
-        registry.addResourceHandler("/webjars/**")
-                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+        registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
         registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
     }
 }
