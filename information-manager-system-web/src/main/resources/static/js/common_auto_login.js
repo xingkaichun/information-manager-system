c0f8bda210b447550153a7d64f0a3eca67ea3ad9
xing kaichun <409060350@qq.com>
2019-09-10 22:50:16 +0800

    bbs

---
+++ information-manager-system-web/src/main/resources/static/js/common_auto_login.js
@@ -0,0 +1,21 @@
+$(function(){
+    var url="http://localhost:80"
+    $.ajax({
+        type: "post",
+        url: url+"/User/GetUserInfo",
+        contentType:"application/json",
+        dataType: "json",
+        async:false,
+        success: function(data){
+            console.log(data)
+            if(data.ServiceCode=='SUCCESS'){
+                var log = "<a href='/bbs/article_list.html' class='nav_li'>论坛</a>"
+                        + "<a href='' class='nav_li'>"+data.Result.UserInfo.UserName+"</a>";
+                $("#rigth_nav").html(log)
+            }
+        },
+        error:function(e){
+
+        }
+    });
+})
