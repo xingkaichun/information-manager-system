cfc78625ffd42a94a7dcade88b30194adaffaec5
xing kaichun <409060350@qq.com>
2019-09-12 21:08:29 +0800

    bbs

--- information-manager-system-web/src/main/resources/static/js/common_auto_login.js
+++ information-manager-system-web/src/main/resources/static/js/common_auto_login.js
@@ -19,19 +19,19 @@ $(function(){
                 if(page == "post_page"){
                     showHtml = "<a href='/bbs/post_list.html' class='nav_li active'>论坛</a>"
                              + "<a href='/bbs/start_a_new_post.html' class='nav_li'>发帖</a>"
-                             + "<a href='/bbs/my_article_list.html' class='nav_li'>我的帖子</a>"
+                             + "<a href='/bbs/my_post_list.html' class='nav_li'>我的帖子</a>"
                              + "<a href='javascript:void(0)' class='nav_li'>"+data.Result.UserInfo.UserName+"</a>";
                 }
                 if(page == "start_a_new_post_page"){
                     showHtml = "<a href='/bbs/post_list.html' class='nav_li'>论坛</a>"
                              + "<a href='/bbs/start_a_new_post.html' class='nav_li active'>发帖</a>"
-                             + "<a href='/bbs/my_article_list.html' class='nav_li'>我的帖子</a>"
+                             + "<a href='/bbs/my_post_list.html' class='nav_li'>我的帖子</a>"
                              + "<a href='javascript:void(0)' class='nav_li'>"+data.Result.UserInfo.UserName+"</a>";
                 }
                 if(page == "my_post_list_page"){
                     showHtml = "<a href='/bbs/post_list.html' class='nav_li'>论坛</a>"
                              + "<a href='/bbs/start_a_new_post.html' class='nav_li'>发帖</a>"
-                             + "<a href='/bbs/my_article_list.html' class='nav_li active'>我的帖子</a>"
+                             + "<a href='/bbs/my_post_list.html' class='nav_li active'>我的帖子</a>"
                              + "<a href='javascript:void(0)' class='nav_li'>"+data.Result.UserInfo.UserName+"</a>";
                 }
                 $("#rigth_nav").html(showHtml)
@@ -41,7 +41,7 @@ $(function(){
                     location.href="/login.html"
                 }
                 if(page == "my_post_list_page"){
-                    alert("只有会员才有'我'的帖子列表页。请先登录。即将跳转到登录页面...");
+                    alert("只有会员才有'我'的帖子页面。请先登录。即将跳转到登录页面...");
                     location.href="/login.html"
                 }
             }
