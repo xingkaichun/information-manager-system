f51302bda7ab297c7b409383ea5af43055f94717
xing kaichun <409060350@qq.com>
2019-09-28 13:40:33 +0800

    update

--- information-manager-system-web/src/main/resources/static/js/book.js
+++ information-manager-system-web/src/main/resources/static/js/book.js
@@ -1,18 +1,32 @@
 $(function () {
     $(document).on("click", ".link_content .rank-one-text", function () {
         var $rank_one = $(this).parents(".rank-one")
-        if ($rank_one.find(".rank-two").is(":hidden")) {
-            $rank_one.find(".rank-two").show()
+        if ($rank_one.attr("is-open")=="false") {
+            // $rank_one.find(".rank-two").show()
+            $rank_one.removeAttr("is-open")
         } else {
-            $rank_one.find(".rank-two").hide()
+            $rank_one.attr("is-open","false")
+            // $rank_one.find(".rank-two").hide()
+        }
+    })
+    $(".rank-one").each(function(){
+        if($(this).find(".rank-two").length){
+            $(this).find(".rank-one-text .link_li").append(`<label><img src="/images/arrows.png"/></label>`)
+        }
+    })
+    $(".rank-two").each(function(){
+        if($(this).find(".rank-three").length){
+            $(this).find(".rank-two-text .link_li").append(`<label><img src="/images/arrows.png"/></label>`)
         }
     })
     $(".link_content .rank-two-text").click(function () {
         var $rank_two = $(this).parents(".rank-two")
-        if ($rank_two.find(".rank-three").is(":hidden")) {
-            $rank_two.find(".rank-three").show()
+        if ($rank_two.attr("is-open")=="false") {
+            // $rank_two.find(".rank-three").show()
+            $rank_two.removeAttr("is-open")
         } else {
-            $rank_two.find(".rank-three").hide()
+            $rank_two.attr("is-open","false")
+            // $rank_two.find(".rank-three").hide()
         }
     })
     $(".link_content .rank-three-text").click(function () {
