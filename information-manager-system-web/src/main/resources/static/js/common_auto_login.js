249ed3d2b537633a7bc221752414adbafc3d132d
xing kaichun <409060350@qq.com>
2019-10-26 16:28:43 +0800

    add

---
+++ information-manager-system-web/src/main/resources/static/js/common_auto_login.js
@@ -0,0 +1,27 @@
+$(function(){
+    var url=""
+
+    $.ajax({
+        type: "post",
+        url: url+"/User/GetUserInfo",
+        contentType:"application/json",
+        dataType: "json",
+        async:false,
+        success: function(data){
+            console.log(data)
+            var jsElement = document.getElementById("common_auto_login_js");
+            var page = jsElement.getAttribute("data");
+            if(data.ServiceCode=='SUCCESS'){
+                var showHtml =`<a href="/home/home.html"><span>${data.Result.UserInfo.UserName}</span></a>`
+                $("#nav").html(showHtml)
+            } else {
+                if(page == "start_a_new_post_page"){
+                    alert("请先登录。确定将跳转到登录页面...");
+                    location.href="/login.html"
+                }
+            }
+        },
+        error:function(e){
+        }
+    });
+})
---
+++ information-manager-system-web/src/main/resources/static/js/login.js
@@ -0,0 +1,60 @@
+$(function(){
+    var url=""
+
+    //提交
+    function verifyFun(params){
+       
+        var email = $("input[name='email']").val()
+        if(email&&email.trim()!=""){
+            var patt=/^\w+((-\w+)|(\.\w+))*\@[A-Za-z0-9]+((\.|-)[A-Za-z0-9]+)*\.[A-Za-z0-9]+$/;
+            if(patt.test(email.trim())){
+                params.email = email.trim()
+            }else{
+                params.verify = "请输入正确邮箱"
+                return params
+            }
+        }else{
+            params.verify = "请输入邮箱"
+            return params
+        }
+
+        var password = $("input[name='password']").val()
+        if(password&&password.trim()!=""){
+            params.password = password.trim()
+        }else{
+            params.verify = "请输入密码"
+            return params
+        }
+        return params
+    }
+    $("#login_btn").click(function(){
+        var params =verifyFun({
+            email:'',
+            password:'',
+            verify:-1
+        })
+        if(params.verify==-1){
+            $.ajax({
+                type: "post",
+                url: url+"/User/Login",
+                data:`{
+                "Email":"${params.email}",
+                "Password":"${params.password}"}`,
+                contentType:"application/json",
+                dataType: "json",
+                success: function(data){
+                    console.log(data)
+                    alert(data.Message)
+                    if(data.ServiceCode=='SUCCESS'){
+                        location.href="/"
+                    }
+                },
+                error:function(e){
+    
+                }
+            });
+        }else{
+            alert(`${params.verify}`)
+        }
+    })
+})
