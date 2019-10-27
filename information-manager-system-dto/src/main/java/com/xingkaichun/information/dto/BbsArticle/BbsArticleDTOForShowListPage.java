6a65bd95cd9e51370b0509d531ad54a178aa899b
xing kaichun <409060350@qq.com>
2019-10-27 20:34:21 +0800

    分页查询帖子返回帖子

--- information-manager-system-dto/src/main/java/com/xingkaichun/information/dto/BbsArticle/BbsArticleDTOForShowListPage.java
+++ information-manager-system-dto/src/main/java/com/xingkaichun/information/dto/BbsArticle/BbsArticleDTOForShowListPage.java
@@ -1,15 +1,37 @@
 package com.xingkaichun.information.dto.BbsArticle;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
 
 @Data
-public class BbsArticleDTOForShowListPage extends BbsArticleDTO {
+public class BbsArticleDTOForShowListPage {
+
+    @JsonProperty("BbsArticleId")
+    private String bbsArticleId;
+
+    @JsonProperty("UserId")
+    private String userId;
+
+    @JsonProperty("Title")
+    private String title;
+
+    @JsonProperty("CreateTime")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private Date createTime;
+
+    @JsonProperty("LastEditTime")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private Date lastEditTime;
 
     @JsonProperty("UserName")
     private String userName;
 
     @JsonProperty("NumberOfComment")
     private int numberOfComment;
-
 }
