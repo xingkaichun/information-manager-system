db37359f5bce884dfc6953e922d8b2917c01be9c
xingkaichun <xingkaichun@ceair.com>
2019-10-29 09:38:09 +0800

    update20191029002

---
+++ information-manager-system-dto/src/main/java/com/xingkaichun/information/dto/BbsArticle/request/QueryBbsArticleCommentByForBbsArticleCommentIdRequest.java
@@ -0,0 +1,14 @@
+package com.xingkaichun.information.dto.BbsArticle.request;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.xingkaichun.common.dto.base.page.PageCondition;
+import lombok.Data;
+
+@Data
+public class QueryBbsArticleCommentByForBbsArticleCommentIdRequest {
+
+    @JsonProperty("ForBbsArticleCommentId")
+    private String forBbsArticleCommentId;
+    @JsonProperty("PageCondition")
+    private PageCondition pageCondition;
+}
