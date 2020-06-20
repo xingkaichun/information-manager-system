c5354d37e2809c64dfeed55860af5e1f069ed8c7
邢开春 <xingkaichun@ceair.com>
2020-06-20 20:34:31 +0800

    异常处理

--- information-manager-system-web/src/main/java/com/xingkaichun/information/controller/MyErrorController.java
+++ information-manager-system-web/src/main/java/com/xingkaichun/information/controller/MyErrorController.java
@@ -2,21 +2,29 @@ package com.xingkaichun.information.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
 
 
 @Controller
 class MyErrorController implements org.springframework.boot.web.servlet.error.ErrorController {
 
     @RequestMapping("/error")
-    public String handleError(HttpServletRequest request){
+    public ModelAndView handleError(HttpServletRequest request,Exception e){
+        ModelAndView modelAndView = new ModelAndView("error");
+        modelAndView.addObject("timestamp",System.currentTimeMillis());
+        modelAndView.addObject("error","StackTrace");
         Integer statusCode = (Integer) request.getAttribute("javax.servlet.error.status_code");
+        modelAndView.addObject("status",statusCode);
+        modelAndView.addObject("message","message");
         if(statusCode == 401 || statusCode == 404 || statusCode == 403){
-            return "error";
         }else{
-            return "error";
         }
+        //TODO 记录异常
+        return modelAndView;
     }
 
     @Override
