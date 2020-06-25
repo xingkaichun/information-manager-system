4230c7b282831b446adfaee7404892155823de85
邢开春 <xingkaichun@ceair.com>
2020-06-25 20:37:18 +0800

    图片上传到文件系统进行统一管理

--- information-manager-system-web/src/main/java/com/xingkaichun/information/InformationManagerSystemWebApplication.java
+++ information-manager-system-web/src/main/java/com/xingkaichun/information/InformationManagerSystemWebApplication.java
@@ -1,16 +1,26 @@
 package com.xingkaichun.information;
 
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 @ServletComponentScan
 @MapperScan("com.xingkaichun.information.dao")
 public class InformationManagerSystemWebApplication {
 
+
 	public static void main(String[] args) {
-		SpringApplication.run(InformationManagerSystemWebApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(InformationManagerSystemWebApplication.class, args);
+
+
+		String fileSystemUploadFileUrl = context.getEnvironment().getProperty("FileSystem.UploadFileUrl");
+		System.setProperty("FileSystem.UploadFileUrl",fileSystemUploadFileUrl);
+
 	}
+
+
 }
--- information-manager-system-web/src/main/java/com/xingkaichun/information/controller/MyErrorController.java
+++ information-manager-system-web/src/main/java/com/xingkaichun/information/controller/MyErrorController.java
@@ -7,8 +7,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.HashMap;
-import java.util.Map;
 
 
 @Controller
